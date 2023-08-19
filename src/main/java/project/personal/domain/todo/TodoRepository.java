5403693c2fc49e9ad3aa98879526271f52023f2c
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-08-19 09:20:12 +0900

    2023/08/19
    TodoRepository has been added, by using Spring Data Jpa, basic CRUD function will be created as a proxy file when it's being run.

---
+++ src/main/java/project/personal/domain/todo/TodoRepository.java
@@ -0,0 +1,20 @@
+package project.personal.domain.todo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    @Query("select t from Todo t where t.registeredDateTime >= :startDateTime " +
+            "and t.registeredDateTime < :endDateTime " +
+            "and t.todoStatus = :todoStatus")
+    List<Todo> findOrdersBy(@Param("startDateTime") LocalDateTime startDateTime,
+                             @Param("endDateTime") LocalDateTime endDateTime,
+                             @Param("todoStatus") TodoStatus todoStatus);
+}
