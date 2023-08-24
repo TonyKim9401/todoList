686b7d3732d52af71cf8ac871570143dca31dcdf
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-08-25 02:10:38 +0900

    2023/08/25
    BaseEntity
    - to use @EnableJpaAuditing properly, details are added on BaseEntity

--- src/main/java/project/personal/domain/BaseEntity.java
+++ src/main/java/project/personal/domain/BaseEntity.java
@@ -1,10 +1,13 @@
 package project.personal.domain;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -16,9 +19,19 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity {
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdDateTime;
 
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
     @LastModifiedDate
+    @Column(updatable = true)
     private LocalDateTime modifiedDateTime;
+
+    @LastModifiedBy
+    @Column(updatable = true)
+    private String modifiedBy;
 
 }
