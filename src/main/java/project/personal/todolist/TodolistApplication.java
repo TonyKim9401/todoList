c67bee1ee96fb38ff2a2391521b214dacb269677
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-08-25 02:12:25 +0900

    2023/08/25
    Main Class
    - @EnableJpaAuditing added to use auto date checking and user infos

--- src/main/java/project/personal/todolist/TodolistApplication.java
+++ src/main/java/project/personal/todolist/TodolistApplication.java
@@ -2,7 +2,10 @@ package project.personal.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class TodolistApplication {
 
