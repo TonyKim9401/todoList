0a1f16b1b9d4598c9d9bd7c6de2b5efcf82f9f1c
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-08-26 00:16:18 +0900

    2023/08/25
    TodoCreateServiceRequest
    
    to transfer data from Controller layer to Service layer

--- src/main/java/project/personal/api/service/todo/request/TodoCreateServiceRequest.java
+++ src/main/java/project/personal/api/service/todo/request/TodoCreateServiceRequest.java
@@ -3,6 +3,7 @@ package project.personal.api.service.todo.request;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import project.personal.domain.user.User;
 
 import java.util.List;
 
@@ -10,4 +11,13 @@ import java.util.List;
 @NoArgsConstructor
 public class TodoCreateServiceRequest {
 
+    private String todoTitle;
+    private String todoContent;
+    private User user;
+
+    @Builder
+    private TodoCreateServiceRequest(String todoTitle, String todoContent) {
+        this.todoTitle = todoTitle;
+        this.todoContent = todoContent;
+    }
 }
