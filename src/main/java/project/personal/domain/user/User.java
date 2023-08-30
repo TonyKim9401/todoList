07bbacdc2f974b3071451708df02da8f9bcb8f87
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-08-30 19:39:34 +0900

    2023/08/30
    User
    @DynamicInsert, @DynamicUpdate added
    id -> userId
    
    RelationMapping
    @OneToMany with todoHistories

--- src/main/java/project/personal/domain/user/User.java
+++ src/main/java/project/personal/domain/user/User.java
@@ -5,8 +5,11 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import project.personal.domain.BaseEntity;
+import project.personal.domain.history.TodoHistory;
 import project.personal.domain.todo.Todo;
 import project.personal.domain.todo.TodoStatus;
 
@@ -18,16 +21,23 @@ import java.util.List;
 @Entity(name = "Users")
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicInsert
+@DynamicUpdate
 public class User extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userId;
     private String nickname;
     private String email;
     private String phoneNumber;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     List<Todo> todos = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    List<TodoHistory> todoHistories = new ArrayList<>();
+
+
 
     @Builder
     private User(String nickname, String email, String phoneNumber) {
