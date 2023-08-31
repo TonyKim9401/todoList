1f66770b37a8589ca11395d35c81a24e044d5a67
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-08-31 23:05:51 +0900

    2023/08/31
    TodoService
    
    saveTodoHistory, saveTodoHistories methods are added

--- src/main/java/project/personal/api/service/todo/TodoService.java
+++ src/main/java/project/personal/api/service/todo/TodoService.java
@@ -5,10 +5,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.personal.api.service.todo.request.TodoCreateServiceRequest;
 import project.personal.api.service.todo.response.TodoResponse;
+import project.personal.domain.history.todo.TodoHistory;
+import project.personal.domain.history.todo.TodoHistoryRepository;
 import project.personal.domain.todo.Todo;
 import project.personal.domain.todo.TodoRepository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,14 +20,18 @@ public class TodoService {
 
     private final TodoRepository todoRepository;
 
+    private final TodoHistoryRepository todoHistoryRepository;
 
     /**
      * 동시성 고민
      * optimistic lock / pessimistic lock / ...
      */
     public TodoResponse createTodo(TodoCreateServiceRequest request) {
-        Todo todo = Todo.create(request.getTodoTitle(), request.getTodoContent());
+        Todo todo = Todo.create(request.toTodoEntity(), null);
         Todo saveTodo = todoRepository.save(todo);
+
+        saveTodoHistory(saveTodo);
+
         return TodoResponse.of(saveTodo);
     }
 
@@ -41,6 +46,26 @@ public class TodoService {
         List<Todo> todos = todoRepository.findAll();
         return todos.stream().map(t -> TodoResponse.of(t))
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * Todo의 Insert, Update 처리 후 TodoHistory 에 이력을 저장한다.
+     * @param todo
+     */
+    public void saveTodoHistory(Todo todo) {
+        TodoHistory todoHistory = TodoHistory.create(todo, null);
+        todoHistoryRepository.save(todoHistory);
+    }
+
+    /**
+     * List<Todo> 의 Insert, Update 처리 후 TodoHistory 에 이력을 저장한다.
+     * @param todoList
+     */
+    public void saveTodoHistories(List<Todo> todoList) {
+        List<TodoHistory> todoHistories = todoList.stream()
+                                                    .map(todo -> TodoHistory.create(todo, null))
+                                                    .toList();
+        todoHistoryRepository.saveAll(todoHistories);
     }
 
 }
