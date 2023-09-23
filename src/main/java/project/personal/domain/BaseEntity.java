20ea6f9c870615c43f11b32684bd13a3a008a515
ktony <k.tony9412@gmail.com>
2023-09-23 22:18:25 +0900

    @MappedSuperclass // 하위 Entity 들이 이 추상객체를 상속받을 것이기 때문에
    @Setter 테스트용
    @Column(updatable = true) 테스트용

--- src/main/java/project/personal/domain/BaseEntity.java
+++ src/main/java/project/personal/domain/BaseEntity.java
@@ -15,13 +15,12 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Getter
-@MappedSuperclass // 하위 Entity 들이 이 추상객체를 상속받을 것이기 때문에
+@MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @CreatedDate
-//    @Setter 테스트용
-//    @Column(updatable = true) 테스트용
+
     @Column(updatable = false)
     private LocalDateTime createdDate;
 
